e6484937522e857a6713fc0bddaea645d452de32
DamiAkinniyi <oluwadamilola.akinniyi@students.iaac.net>
2022-09-28 08:51:03 -0500

    initialising

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import image from './assets/dami_akinniyi-02.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={image} className="App-logo" alt="Dami Akinniyi" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Computational Designer | Machine Learning Scientist
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Explore Project
         </a>
       </header>
     </div>
